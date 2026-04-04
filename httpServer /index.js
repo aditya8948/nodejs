788935afe4aeec61461b261086c84028fe43b8c2
aditya8948 <89adityapandey@gmail.com>
2026-04-04 11:59:29 +0530

    url parsing

--- httpServer /index.js
+++ httpServer /index.js
@@ -1,7 +1,7 @@
-// creating asic http server 
+// creating basic http server 
 
 const http = require('http');
-const { json } = require('stream/consumers');
+const { url } = require('inspector');
 
 // const server = http.createServer((req , res) => {
 //     console.log(req); // this will have information aout user , like ip , time etc 
@@ -30,3 +30,32 @@ const server = http.createServer((req , res) => {
 
 server.listen(3000 , () => console.log("server started"));
 
+
+// url -> universal resource locater 
+// url handing -> means understanding what user requested 
+
+// OLD WAY 
+// import url module 
+// const req = "localHost:3000/api?id:10&name:aditya";
+// const url = require("url");
+// const myUrl = url.parse(req, true);
+// console.log(myUrl.pathname);
+// console.log(myUrl.query);
+
+// modern way  
+// const myUrl = new URL("http://localhost:3000/api?id=10&name=aditya");
+
+// console.log(myUrl.pathname);        // /api
+// console.log(myUrl.searchParams.get("id"));   // 10
+// console.log(myUrl.searchParams.get("name")); // aditya
+
+
+// extract information from server request 
+http.createServer((req, res) => {
+  const myUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  console.log(myUrl.pathname);
+  console.log(Object.fromEntries(myUrl.searchParams));
+
+  res.end("Done");
+}).listen(4000);
