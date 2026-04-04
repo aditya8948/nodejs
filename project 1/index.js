09068b9bc3fea13a8b0b07f0436d169ef031a3d8
aditya8948 <89adityapandey@gmail.com>
2026-04-05 03:16:40 +0530

    use get method

--- project 1/index.js
+++ project 1/index.js
@@ -1,6 +1,34 @@
-const express = recquire("express");
-
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.listen(port , ()=> console.log(`server runing at port ${port}`));
+
+// Load sample users from MOCK_DATA.json
+const users = require("./MOCK_DATA.json");
+
+// ROUTES
+// This route shows users in simple HTML.
+app.get("/users", (req, res) => {
+ const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name} ${user.last_name}</li>`).join("")}
+    </ul>
+  `;
+    return res.send(html);
+});
+
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+// Dynamic route parameter
+// Example: /api/users/1
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
+
+
+app.listen(port, () => console.log(`Server running at port ${port}`));
